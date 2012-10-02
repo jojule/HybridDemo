2cee193c568dfd010f9f30b0d5a82cadb4b60d21
Joonas Lehtinen <joonas@vaadin.com>
2012-10-02 00:31:31 -0700

    Reduced number of ajax calls

--- src/main/java/org/vaadin/demo/hybrid/client/HourListServerRpc.java
+++ src/main/java/org/vaadin/demo/hybrid/client/HourListServerRpc.java
@@ -2,10 +2,11 @@ package org.vaadin.demo.hybrid.client;
 
 import org.vaadin.demo.hybrid.client.HourListState.HourEntry;
 
+import com.vaadin.shared.annotations.Delayed;
 import com.vaadin.shared.communication.ServerRpc;
 
 public interface HourListServerRpc extends ServerRpc {
-
+	@Delayed
     public void hourUpdated(HourEntry hour, int row);
 
 }
